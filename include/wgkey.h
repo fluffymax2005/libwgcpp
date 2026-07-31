66a3273f0935051f2d8685c6bbc23839f3ce7267
fluffymax2005 <santech_montage@mail.ru>
2026-07-31 10:45:21 +0300

    WgKey base class implemented

---
+++ include/wgkey.h
@@ -0,0 +1,30 @@
+#ifndef WGKEY_H
+#define WGKEY_H
+
+#include "wireguard.h"
+
+#include <cstdint>
+#include <memory>
+#include <array>
+
+constexpr uint32_t WG_KEY_LEN = sizeof(wg_key);
+
+class WgKey {
+public:
+    virtual ~WgKey() = default;
+
+    virtual bool isProper() const noexcept = 0;
+    virtual void generate() = 0;
+
+    const uint8_t* data() const noexcept;
+    uint32_t size() const noexcept;
+    std::shared_ptr<std::array<uint8_t, WG_KEY_LEN>> cloneData() const noexcept;
+    bool initialize() noexcept;
+
+protected:
+    std::shared_ptr<std::array<uint8_t, WG_KEY_LEN>> key{nullptr};
+
+    bool isGenerated{false};
+};
+
+#endif // WGKEY_H
---
+++ src/wgkey.cpp
@@ -0,0 +1,23 @@
+#include "wgkey.h"
+
+
+const uint8_t* WgKey::data() const noexcept {
+    return key ? key->data() : nullptr;
+}
+
+uint32_t WgKey::size() const noexcept {
+    return WG_KEY_LEN;
+}
+
+std::shared_ptr<std::array<uint8_t, WG_KEY_LEN>> WgKey::cloneData() const noexcept {
+    if (key == nullptr)
+        return nullptr;
+    return std::make_shared<std::array<uint8_t, WG_KEY_LEN>>(*key);
+}
+
+bool WgKey::initialize() noexcept {
+    if (key)
+        return false;
+    key = std::make_shared<std::array<uint8_t, WG_KEY_LEN>>();
+    return key->empty();
+}
