f426ea8662b3fe8b698fc4a4770f630dd4c8c079
fluffymax2005 <santech_montage@mail.ru>
2026-08-04 14:09:57 +0300

    Creating example to create interface "wg0" then set peer and get it

---
+++ examples/example1.cpp
@@ -0,0 +1,48 @@
+#include "wginterface.h"
+#include <iostream>
+
+int main() {
+
+    const char* interfaceName = "wg0";
+
+    try {
+
+        // Create peer
+        WgPeer peer;
+
+        WgPrivateKey peerPrKey;
+        peerPrKey.generate();
+        WgPublicKey peerPbKey(peerPrKey);
+        peerPbKey.generate();
+
+        peer.setPublicKey(std::move(peerPbKey));
+
+        // Create interface
+        WgInterface interface(interfaceName);
+        interface.setListeningPort(1234);
+        interface.addPeer(std::move(peer));
+
+        WgPrivateKey interfacePrKey;
+        interfacePrKey.generate();
+
+        interface.setPrivateKey(std::move(interfacePrKey));
+
+        interface.set();
+
+        // Print
+        const auto peers = interface.getPeers();
+        std::cout << "Interface \"" << interfaceName << "\" has "<< peers.size() << " peers" << std::endl;
+        for (const auto& str : peers) {
+            std::cout << "Interface \"" << interfaceName << "\" has peer with public key \"" << str << "\"" << std::endl;
+        }
+
+    } catch (const WgException& e) {
+        std::cout << e.what() << std::endl;
+    } catch (const std::runtime_error& e) {
+        std::cout << e.what() << std::endl;
+    }
+
+
+
+    return 0;
+}
